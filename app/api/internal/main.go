f0f2cd2ab01d6b4c4f1847617566640879a4c669
bivol <bivol@192.168.1.16>
2026-01-20 15:35:48 +0200

    am facut ceva progres merge sql

---
+++ app/api/internal/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "net/http"
+
+func main(){
+	
+	http.HandleFunc("/health", func(w http.ResponseWriter,r *http.Request){ w.WriteHeader(200); w.Write([]byte("ok")) })
+	http.ListenAndServe(":8080",nil)
+}
