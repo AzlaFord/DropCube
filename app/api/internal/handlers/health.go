7938d04331a62687bebfef32e59845399ed35b41
bivol <vbivol394@gmail.com>
2026-02-02 13:46:02 +0200

    adugat middleware

--- app/api/internal/handlers/health.go
+++ app/api/internal/handlers/health.go
@@ -1,6 +1,9 @@
 package handlers
 
-import "net/http"
+import (
+	"net/http"
+)
+
 
 func HealthHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(200)
--- app/api/main.go
+++ app/api/main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"dropcube-api/internal/http/handlers"
+	"dropcube-api/internal/handlers"
+	"dropcube-api/internal/middleware"
 	"log"
 	"net/http"
 )
@@ -9,9 +10,10 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/health", handlers.HealthHandler)
+	loggedMux := middleware.Middleware(mux)
 
 	log.Println("Server running on :8080")
-	if err := http.ListenAndServe(":8080", mux); err != nil {
+	if err := http.ListenAndServe(":8080", loggedMux); err != nil {
 		log.Fatalf("Server failed: %v", err)
 	}
 }
