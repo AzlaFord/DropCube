46c2b7d4960fc435e6cb9151bd5b45618f3d74de
bivol <vbivol394@gmail.com>
2026-02-04 14:23:03 +0200

    upload api route

--- app/api/internal/handlers/upload.go
+++ app/api/internal/handlers/upload.go
@@ -1,13 +1,23 @@
 package handlers
 
-import "net/http"
+import (
+	"net/http"
+)
 
 func Upload(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.WriteHeader(http.StatusMethodNotAllowed)
-		w.Write([]byte("method not allowed"))
+		w.Write([]byte("Method not allowed"))
 		return
 	}
-	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte("Upload"))
+	_, multipartHeader, errfile := r.FormFile("file")
+	if errfile != nil || multipartHeader.Filename == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if multipartHeader.Filename != "" {
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte("Upload"))
+	}
+
 }
