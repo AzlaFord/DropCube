38a3e1cfa7ab7d2fca25cab8e0e59dac68da6e0e
bivol <vbivol394@gmail.com>
2026-02-09 11:42:22 +0200

    conecatare baza de date

--- app/api/internal/db/db.go
+++ app/api/internal/db/db.go
@@ -2,5 +2,22 @@ package db
 
 import (
 	"database/sql"
+	"os"
 )
 
+func ConnectDB() (*sql.DB, error) {
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	name := os.Getenv("DB_NAME")
+	db, err := sql.Open("postgres", "postgres://"+user+":"+password+"@"+host+":"+port+"/"+name+"?sslmode=disable")
+	if err != nil {
+		return db, err
+	}
+	errPing := db.Ping()
+	if errPing != nil {
+		return db, errPing
+	}
+	return db, nil
+}
