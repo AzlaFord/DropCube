91a70d0b03210eac2e753b95104f47d977c0490d
AzlaFord <vbivol394@gmail.com>
2026-04-21 12:27:01 +0300

    detectarea tipului de file

--- app/api/internal/handlers/getFileDown.go
+++ app/api/internal/handlers/getFileDown.go
@@ -10,11 +10,14 @@ import (
 	"strings"
 )
 
-func GetFile(db *sql.DB) http.HandlerFunc {
+func GetFileDown(db *sql.DB) http.HandlerFunc {
+
 	return func(w http.ResponseWriter, r *http.Request) {
+
+		buf := make([]byte, 512)
 		var filepath string
 		var filename string
-		w.Header().Set("Content-Type", "application/octet-stream")
+
 		fmt.Println("GET FILE:", r.URL.Path)
 		parts := strings.Split(r.URL.Path, "/")
 		idStr := parts[len(parts)-1]
@@ -35,22 +38,32 @@ func GetFile(db *sql.DB) http.HandlerFunc {
 		errScan := row.Scan(&filepath, &filename)
 
 		if errScan != nil {
-			w.WriteHeader(405)
-			w.Write([]byte("bad request"))
+			w.WriteHeader(500)
 			return
 		}
 		fmt.Println("PATH:", filepath)
 		fmt.Println("NAME:", filename)
 		file, err := os.Open(filepath)
 		if err != nil {
-			w.WriteHeader(405)
-			w.Write([]byte("bad request"))
+			w.WriteHeader(404)
+			w.Write([]byte("Not Found"))
 			return
 		}
 		defer file.Close()
-		w.WriteHeader(200)
-		w.Write([]byte("ok"))
+
+		n, err := file.Read(buf)
+		if err != nil && err != io.EOF {
+			w.WriteHeader(500)
+			return
+		}
+
+		file.Seek(0, 0)
+		fileType := http.DetectContentType(buf[:n])
+
+		w.Header().Set("Content-Type", fileType)
+
 		w.Header().Set("Content-Disposition", "attachment; filename="+filename)
+		w.WriteHeader(200)
 		io.Copy(w, file)
 	}
 }
