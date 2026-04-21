44c2a17ab831a714ac8b58e075720004d25c1980
AzlaFord <vbivol394@gmail.com>
2026-04-21 11:38:22 +0300

    downlaod functional

--- app/api/internal/handlers/getFile.go
+++ app/api/internal/handlers/getFile.go
@@ -3,7 +3,11 @@ package handlers
 import (
 	"database/sql"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func GetFile(db *sql.DB) http.HandlerFunc {
@@ -12,27 +16,36 @@ func GetFile(db *sql.DB) http.HandlerFunc {
 		var filename string
 		w.Header().Set("Content-Type", "application/json")
 		fmt.Println("GET FILE:", r.URL.Path)
-		id := r.URL.PATH[:-1]
+		parts := strings.Split(r.URL.Path, "/")
+		idStr := parts[len(parts)-1]
+		id, err := strconv.Atoi(idStr)
+
+		if err != nil {
+			w.WriteHeader(405)
+			w.Write([]byte("bad request"))
+			return
+		}
 		if r.Method != http.MethodGet {
 			w.WriteHeader(405)
 			w.Write([]byte("bad request"))
 			return
 		}
 		row := db.QueryRow("SELECT filepath, filename FROM files WHERE id = $1", id)
-		if err != nil {
-			w.WriteHeader(405)
-			w.Write([]byte("bad request"))
-			return
-		}
 
-		defer row.Close()
-		err := row.Scan(&filepath, &filename)
-		if err != nil {
+		errScan := row.Scan(&filepath, &filename)
+
+		if errScan != nil {
 			w.WriteHeader(405)
 			w.Write([]byte("bad request"))
 			return
 		}
 		fmt.Println("PATH:", filepath)
 		fmt.Println("NAME:", filename)
+		file, err := os.Open(filepath)
+		defer file.Close()
+		w.Header().Set("Content-Disposition", "attachment; filename="+filename)
+		io.Copy(w, file)
+		w.WriteHeader(200)
+		w.Write([]byte("ok"))
 	}
 }
